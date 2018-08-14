3cf0cc008d420ed8a86aedc017e6cb4363bdfb9e
Popov Evgeniy <tuzik87@inbox.ru>
2018-08-14 20:07:31 +0600

    Small changes

--- ORGF.cpp
+++ ORGF.cpp
@@ -178,7 +178,7 @@ void ORGF_Engine::prepare_engine()
   puts("Can't load the standart cursor");
   exit(EXIT_FAILURE);
  }
- if (RegisterClass(&window_class)==0)
+ if (!RegisterClass(&window_class))
  {
   puts("Can't register window class");
   exit(EXIT_FAILURE);
@@ -1541,7 +1541,19 @@ void ORGF_Canvas::resize_image(const unsigned long int new_width,const unsigned 
  height=new_height;
 }
 
-void ORGF_Background::draw_background_image(const unsigned long int start,const unsigned long int frame_width,const unsigned long int frame_height)
+ORGF_Background::ORGF_Background()
+{
+ start=0;
+ frame_width=0;
+ frame_height=0;
+}
+
+ORGF_Background::~ORGF_Background()
+{
+
+}
+
+void ORGF_Background::draw_background_image()
 {
  unsigned long int x,y;
  size_t offset;
@@ -1559,18 +1571,18 @@ void ORGF_Background::draw_background_image(const unsigned long int start,const 
 
 void ORGF_Background::draw_horizontal_background(const unsigned long int frame)
 {
- unsigned long int start,frame_width;
  frame_width=width/frames;
+ frame_height=height;
  start=(frame-1)*frame_width;
- this->draw_background_image(start,frame_width,height);
+ this->draw_background_image();
 }
 
 void ORGF_Background::draw_vertical_background(const unsigned long int frame)
 {
- unsigned long int start,frame_height;
+ frame_width=width;
  frame_height=height/frames;
  start=(frame-1)*frame_height*width;
- this->draw_background_image(start,width,frame_height);
+ this->draw_background_image();
 }
 
 void ORGF_Background::draw_background()
