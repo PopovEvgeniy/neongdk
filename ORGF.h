7fd6a08c4117e1fc8fd1666aa5bbf703e8934602
Popov Evgeniy <tuzik87@inbox.ru>
2019-10-18 19:58:42 +0700

    Small changes

--- ORGF.h
+++ ORGF.h
@@ -451,10 +451,10 @@ class Surface
 {
  private:
  Screen *surface;
+ IMG_Pixel *image;
  unsigned long int width;
  unsigned long int height;
  protected:
- IMG_Pixel *image;
  void save();
  void restore();
  void clear_buffer();
@@ -462,8 +462,10 @@ class Surface
  void load_from_buffer(Image &buffer);
  void set_width(const unsigned long int image_width);
  void set_height(const unsigned long int image_height);
+ void set_buffer(IMG_Pixel *buffer);
  size_t get_offset(const unsigned long int start,const unsigned long int x,const unsigned long int y);
  void draw_image_pixel(const size_t offset,const unsigned long int x,const unsigned long int y);
+ bool compare_pixels(const size_t first,const size_t second);
  public:
  Surface();
  ~Surface();
@@ -523,7 +525,6 @@ class Sprite:public Canvas
  unsigned long int sprite_width;
  unsigned long int sprite_height;
  SPRITE_TYPE current_kind;
- bool compare_pixels(const IMG_Pixel &first,const IMG_Pixel &second);
  void draw_sprite_pixel(const size_t offset,const unsigned long int x,const unsigned long int y);
  public:
  Sprite();
