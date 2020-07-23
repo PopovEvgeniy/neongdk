2029afd9beccc132c7da12b78005611c05cb584e
Popov Evgeniy <tuzik87@inbox.ru>
2020-07-24 00:27:11 +0700

    Small changes

--- neongdk.cpp
+++ neongdk.cpp
@@ -139,10 +139,12 @@ Engine::Engine()
 {
  window_class.lpszClassName=TEXT("NEONGDK");
  window_class.style=CS_HREDRAW|CS_VREDRAW;
+ window_class.cbSize=sizeof(WNDCLASSEX);
  window_class.lpfnWndProc=Process_Message;
  window_class.hInstance=NULL;
  window_class.hbrBackground=NULL;
  window_class.hIcon=NULL;
+ window_class.hIconSm=NULL;
  window_class.hCursor=NULL;
  window_class.cbClsExtra=0;
  window_class.cbWndExtra=0;
@@ -175,6 +177,16 @@ void Engine::set_backgrond_color()
 
 }
 
+void Engine::load_icon()
+{
+ window_class.hIcon=LoadIcon(NULL,IDI_APPLICATION);
+ if (window_class.hIcon==NULL)
+ {
+  Halt("Can't load the standart program icon");
+ }
+
+}
+
 void Engine::load_cursor()
 {
  window_class.hCursor=LoadCursor(NULL,IDC_ARROW);
@@ -187,7 +199,7 @@ void Engine::load_cursor()
 
 void Engine::register_window_class()
 {
- if (!RegisterClass(&window_class))
+ if (!RegisterClassEx(&window_class))
  {
   Halt("Can't register window class");
  }
@@ -208,6 +220,7 @@ void Engine::prepare_engine()
 {
  this->get_instance();
  this->set_backgrond_color();
+ this->load_icon();
  this->load_cursor();
  this->register_window_class();
 }
