1a27510c2f31c351b8893c7f891f23225fd870c0
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2023-05-20 19:06:50 +0700

    Source code synced with Eugene game development kit 6.9.2

--- source/neongdk.cpp
+++ source/neongdk.cpp
@@ -976,6 +976,7 @@ namespace NEONGDK
 
   void Render::set_common_setting()
   {
+   glShadeModel(GL_FLAT);
    glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
    glAlphaFunc(GL_GREATER,0.1f);
    glClearColor(0.0,0.0,0.0,0.0);
