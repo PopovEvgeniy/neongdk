96a944e0a91db5fab308e797dc66a341a8009163
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-01-05 22:38:42 +0300

    Source code synced with Eugene game development kit 7.8.8

--- source/neongdk.cpp
+++ source/neongdk.cpp
@@ -2833,6 +2833,16 @@ namespace NEONGDK
    return this;
   }
 
+  bool Sheet::check_row(const unsigned int target) const
+  {
+   return (target>0) && (target<=rows);
+  }
+
+  bool Sheet::check_column(const unsigned int target) const
+  {
+   return (target>0) && (target<=columns);
+  }
+
   unsigned int Sheet::get_row(const unsigned int target) const
   {
    unsigned int row;
@@ -2871,9 +2881,9 @@ namespace NEONGDK
   {
    unsigned int target;
    target=1;
-   if ((row>0)&&(row<=rows))
+   if (this->check_row(row)==true)
    {
-    if ((column>0)&&(column<=columns))
+    if (this->check_column(column)==true)
     {
      target+=(row-1)+(column-1)*rows;
     }
@@ -2931,9 +2941,9 @@ namespace NEONGDK
 
   void Sheet::select(const unsigned int row,const unsigned int column)
   {
-   if ((row>0)&&(row<=rows))
+   if (this->check_row(row)==true)
    {
-    if ((column>0)&&(column<=columns))
+    if (this->check_column(column)==true)
     {
      billboard.set_tile_offset(static_cast<double>(row),static_cast<double>(rows),static_cast<double>(column),static_cast<double>(columns));
     }
