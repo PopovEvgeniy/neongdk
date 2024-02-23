3a245c885375a361bd26ce1c8e99180a244b0340
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-02-23 21:28:27 +0700

    Source code synced with Eugene game development kit 8.3.4

--- source/neongdk.cpp
+++ source/neongdk.cpp
@@ -3216,6 +3216,11 @@ namespace NEONGDK
    return stage.is_last_frame();
   }
 
+  bool Background::check_frame(const unsigned int target) const
+  {
+   return stage.check_frame(target);
+  }
+
   unsigned int Background::get_frame() const
   {
    return stage.get_frame();
