6a727efdb8179631502fdf0184ff913520533b57
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-04-22 19:04:01 +0700

    Source code synced with Eugene game development kit 8.5.8

--- source/neongdk.cpp
+++ source/neongdk.cpp
@@ -1951,6 +1951,15 @@ namespace NEONGDK
    original.destroy_buffer();
   }
 
+  void Image::flip_image(const Core::MIRROR_KIND mirror)
+  {
+   if (data.get_buffer()!=NULL)
+   {
+    this->mirror_image(mirror);
+   }
+
+  }
+
   void Image::uncompress_tga_data(const unsigned char *target)
   {
    size_t index,position,amount;
@@ -2079,29 +2088,17 @@ namespace NEONGDK
 
   void Image::horizontal_mirror()
   {
-   if (data.get_buffer()!=NULL)
-   {
-    this->mirror_image(Core::HORIZONTAL_MIRROR);
-   }
-
+   this->flip_image(Core::HORIZONTAL_MIRROR);
   }
 
   void Image::vertical_mirror()
   {
-   if (data.get_buffer()!=NULL)
-   {
-    this->mirror_image(Core::VERTICAL_MIRROR);
-   }
-
+   this->flip_image(Core::VERTICAL_MIRROR);
   }
 
   void Image::complex_mirror()
   {
-   if (data.get_buffer()!=NULL)
-   {
-    this->mirror_image(Core::MIRROR_BOTH);
-   }
-
+   this->flip_image(Core::MIRROR_BOTH);
   }
 
   void Image::destroy_image()
