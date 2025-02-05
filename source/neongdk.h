77e1f5e6d062487dfb2a5d930c1390d123c20236
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-02-05 23:56:43 +0700

    The source code synced with the Eugene game development kit 9.4.9

--- source/neongdk.h
+++ source/neongdk.h
@@ -406,6 +406,7 @@ typedef enum
    void set_tile_offset(const float row,const float rows,const float column,const float columns);
    void set_horizontal_offset(const float current,const float total);
    void set_vertical_offset(const float current,const float total);
+   void set_texture_coordinates(const size_t index,const float u,const float v);
   };
 
   class Rectangle:public Shape
@@ -683,6 +684,7 @@ typedef enum
    public:
    Billboard();
    ~Billboard();
+   void set_texture_coordinates(const size_t index,const float u,const float v);
    void set_transparent(const bool enabled);
    bool get_transparent() const;
    void set_width(const unsigned int width);
