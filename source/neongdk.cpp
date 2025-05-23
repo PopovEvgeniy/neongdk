c3841ff7566e6c9e3d3b14b768e5385eb66774c3
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-05-23 21:43:36 +0700

    The source code synced with the Black game development kit 7.3.8

--- source/neongdk.cpp
+++ source/neongdk.cpp
@@ -2011,9 +2011,13 @@ namespace NEONGDK
   {
    unsigned int highest_x;
    highest_x=x_offset+viewport_width;
-   if (highest_x>screen_width)
+   if (highest_x>=highest_x_offset)
    {
     highest_x=screen_width;
+   }
+   if (highest_x>screen_width)
+   {
+    highest_x=x_offset;
    }
    return highest_x;
   }
@@ -2022,9 +2026,13 @@ namespace NEONGDK
   {
    unsigned int highest_y;
    highest_y=y_offset+viewport_height;
-   if (highest_y>screen_height)
+   if (highest_y>=highest_y_offset)
    {
     highest_y=screen_height;
+   }
+   if (highest_y>screen_height)
+   {
+    highest_y=y_offset;
    }
    return highest_y;
   }
@@ -2089,11 +2097,8 @@ namespace NEONGDK
 
   unsigned int Camera::set_x(const unsigned int x)
   {
-   if (x<highest_x_offset)
-   {
-    x_offset=x;
-   }
-   else
+   x_offset=x;
+   if (x_offset>highest_x_offset)
    {
     x_offset=highest_x_offset;
    }
@@ -2102,11 +2107,8 @@ namespace NEONGDK
 
   unsigned int Camera::set_y(const unsigned int y)
   {
-   if (y<highest_y_offset)
-   {
-    y_offset=y;
-   }
-   else
+   y_offset=y;
+   if (y_offset>highest_y_offset)
    {
     y_offset=highest_y_offset;
    }
