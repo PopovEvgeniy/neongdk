007c35c37d77373fc9092b1e7a617f0bd2aabdfb
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-10-19 20:38:57 +0700

    The source code synced with the Eugene Game Development Kit 9.7.6

--- source/neongdk.cpp
+++ source/neongdk.cpp
@@ -999,7 +999,6 @@ namespace NEONGDK
    if (glIsEnabled(GL_ALPHA_TEST)==GL_FALSE)
    {
     glEnable(GL_ALPHA_TEST);
-    glEnable(GL_BLEND);
    }
 
   }
@@ -1009,7 +1008,6 @@ namespace NEONGDK
    if (glIsEnabled(GL_ALPHA_TEST)==GL_TRUE)
    {
     glDisable(GL_ALPHA_TEST);
-    glDisable(GL_BLEND);
    }
 
   }
@@ -1085,13 +1083,12 @@ namespace NEONGDK
 
   void Render::set_perfomance_settings()
   {
-   glDisable(GL_POINT_SMOOTH);
-   glDisable(GL_LINE_SMOOTH);
-   glDisable(GL_POLYGON_SMOOTH);
-   glDisable(GL_POLYGON_OFFSET_POINT);
-   glDisable(GL_POLYGON_OFFSET_LINE);
-   glDisable(GL_POLYGON_OFFSET_FILL);
-   glDisable(GL_POLYGON_STIPPLE);
+   glDisable(GL_TEXTURE_1D);
+   glDisable(GL_BLEND);
+   glDisable(GL_TEXTURE_GEN_Q);
+   glDisable(GL_TEXTURE_GEN_R);
+   glDisable(GL_TEXTURE_GEN_S);
+   glDisable(GL_TEXTURE_GEN_T);
    glDisable(GL_DITHER);
    glDisable(GL_LOGIC_OP);
    glDisable(GL_INDEX_LOGIC_OP);
@@ -1103,12 +1100,14 @@ namespace NEONGDK
    glDisable(GL_NORMALIZE);
    glDisable(GL_AUTO_NORMAL);
    glDisable(GL_COLOR_MATERIAL);
-   glDisable(GL_TEXTURE_GEN_Q);
-   glDisable(GL_TEXTURE_GEN_R);
-   glDisable(GL_TEXTURE_GEN_S);
-   glDisable(GL_TEXTURE_GEN_T);
-   glDisable(GL_TEXTURE_1D);
    glDisable(GL_CULL_FACE);
+   glDisable(GL_POINT_SMOOTH);
+   glDisable(GL_LINE_SMOOTH);
+   glDisable(GL_POLYGON_SMOOTH);
+   glDisable(GL_POLYGON_OFFSET_POINT);
+   glDisable(GL_POLYGON_OFFSET_LINE);
+   glDisable(GL_POLYGON_OFFSET_FILL);
+   glDisable(GL_POLYGON_STIPPLE);
    glDisable(GL_MAP1_COLOR_4);
    glDisable(GL_MAP1_INDEX);
    glDisable(GL_MAP1_NORMAL);
@@ -1129,7 +1128,6 @@ namespace NEONGDK
    glDisable(GL_MAP2_VERTEX_4);
    glEnable(GL_TEXTURE_2D);
    glEnable(GL_ALPHA_TEST);
-   glEnable(GL_BLEND);
    glEnableClientState(GL_VERTEX_ARRAY);
    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
    glDisableClientState(GL_COLOR_ARRAY);
@@ -1152,7 +1150,6 @@ namespace NEONGDK
    glDrawBuffer(GL_BACK);
    glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
    glShadeModel(GL_FLAT);
-   glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
    glAlphaFunc(GL_GREATER,0.6f);
    glClearColor(0.0,0.0,0.0,0.0);
   }
