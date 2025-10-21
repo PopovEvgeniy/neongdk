6f6093a3e00b8d36b3c1cab72b68cc67dc98c993
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-10-21 22:25:55 +0700

    The source code synced with the Eugene Game Development Kit 9.7.7

--- source/neongdk.cpp
+++ source/neongdk.cpp
@@ -2957,10 +2957,11 @@ namespace NEONGDK
    return this->load(picture,kind,frames);
   }
 
-  void Sprite::set_target(const unsigned int target)
+  unsigned int Sprite::set_target(const unsigned int target)
   {
    this->set_frame(target);
    this->set_sprite_frame();
+   return this->get_frame();
   }
 
   void Sprite::step()
@@ -3208,14 +3209,11 @@ namespace NEONGDK
 
   }
 
-  void Sheet::set_target(const unsigned int target)
+  unsigned int Sheet::set_target(const unsigned int target)
   {
    this->set_frame(target);
-   if (this->check_frame(target)==true)
-   {
-    this->select(this->get_row(target),this->get_column(target));
-   }
-
+   this->select(this->get_row(this->get_frame()),this->get_column(this->get_frame()));
+   return this->get_frame();
   }
 
   void Sheet::step()
