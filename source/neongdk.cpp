d3aa269f04b5b7325b52b98244762f65abf5a88b
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-11-08 21:39:28 +0700

    The source code synced with the Eugene Game Development Kit 9.8.6

--- source/neongdk.cpp
+++ source/neongdk.cpp
@@ -1834,7 +1834,7 @@ namespace NEONGDK
 
   }
 
-  bool Screen::update()
+  bool Screen::sync(const bool limit)
   {
    if (this->get_context()!=NULL)
    {
@@ -1842,22 +1842,21 @@ namespace NEONGDK
     this->update_counter();
     this->clear_stage();
    }
+   if (limit==true)
+   {
+    this->wait_timer();
+   }
    return this->process_message();
   }
 
   bool Screen::sync()
   {
-   this->wait_timer();
-   return this->update();
+   return this->sync(true);
   }
 
-  bool Screen::sync(const bool limit)
+  bool Screen::update()
   {
-   if (limit==true)
-   {
-    this->wait_timer();
-   }
-   return this->update();
+   return this->sync(false);
   }
 
   bool Screen::is_ready()
