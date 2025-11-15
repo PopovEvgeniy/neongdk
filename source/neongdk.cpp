40e897038a26235ba5ab99521547981360dcbe87
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-11-16 01:48:15 +0700

    The source code synced with the Eugene Game Development Kit 9.9.2

--- source/neongdk.cpp
+++ source/neongdk.cpp
@@ -997,7 +997,6 @@ namespace NEONGDK
    if (glIsEnabled(GL_ALPHA_TEST)==GL_FALSE)
    {
     glEnable(GL_ALPHA_TEST);
-    glEnable(GL_BLEND);
    }
 
   }
@@ -1007,7 +1006,6 @@ namespace NEONGDK
    if (glIsEnabled(GL_ALPHA_TEST)==GL_TRUE)
    {
     glDisable(GL_ALPHA_TEST);
-    glDisable(GL_BLEND);
    }
 
   }
@@ -1084,6 +1082,7 @@ namespace NEONGDK
   void Render::set_perfomance_settings()
   {
    glDisable(GL_TEXTURE_1D);
+   glDisable(GL_BLEND);
    glDisable(GL_TEXTURE_GEN_Q);
    glDisable(GL_TEXTURE_GEN_R);
    glDisable(GL_TEXTURE_GEN_S);
@@ -1126,7 +1125,6 @@ namespace NEONGDK
    glDisable(GL_MAP2_VERTEX_3);
    glDisable(GL_MAP2_VERTEX_4);
    glEnable(GL_TEXTURE_2D);
-   glEnable(GL_BLEND);
    glEnable(GL_ALPHA_TEST);
    glEnableClientState(GL_VERTEX_ARRAY);
    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
@@ -1150,7 +1148,6 @@ namespace NEONGDK
    glDrawBuffer(GL_BACK);
    glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
    glShadeModel(GL_FLAT);
-   glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
    glAlphaFunc(GL_GREATER,0.6f);
    glClearColor(0.0,0.0,0.0,0.0);
   }
