07d3f89230cea70066cb2060f9dc446d8194abe6
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2026-01-07 01:35:41 +0700

    The source code synced with the Black Game Development Kit 7.9.2

--- source/neongdk.h
+++ source/neongdk.h
@@ -4,7 +4,7 @@ Some code taken from wglext.h(https://www.khronos.org/registry/OpenGL/api/GL/wgl
 
 The Neon Game Development Kit license
 
-Copyright (C) 2017 - 2025 Popov Evgeniy Alekseyevich
+Copyright (C) 2017 - 2026 Popov Evgeniy Alekseyevich
 
 This software is provided 'as-is', without any express or implied
 warranty.  In no event will the authors be held liable for any damages
@@ -833,6 +833,24 @@ typedef enum
    void destroy();
    void clone(Ribbon *target);
    void clone(Ribbon &target);
+  };
+
+  class Tier:public Billboard,public Animation,public Picture
+  {
+   private:
+   void set_sprite_frame();
+   public:
+   Tier();
+   ~Tier();
+   Tier* get_handle();
+   bool load(Image *buffer,const unsigned int frames);
+   bool load(Image &buffer,const unsigned int frames);
+   bool load(const char *name,const unsigned int frames);
+   unsigned int set_target(const unsigned int target);
+   void step();
+   void destroy();
+   void clone(Tier *target);
+   void clone(Tier &target);
   };
 
   class Cartoon:public Billboard,public Picture
