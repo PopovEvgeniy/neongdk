adf766bbb1b0ff3f5cf718f684fc274eba51942e
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2026-01-22 02:45:39 +0700

    The source code synced with the Eugene Game Development Kit 10.1.9

--- source/neongdk.cpp
+++ source/neongdk.cpp
@@ -2355,6 +2355,11 @@ namespace NEONGDK
    this->set_size(0,0);
   }
 
+  size_t Image::get_pixel_amount() const
+  {
+   return length/3;
+  }
+
   size_t Image::get_length() const
   {
    return length;
